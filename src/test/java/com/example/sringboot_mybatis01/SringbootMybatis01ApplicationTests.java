485e1147b3a86ffd6dc86563959278b208d13e1b
anhao1 <48540217+anhao1@users.noreply.github.com>
2019-11-11 18:19:40 +0800

    Initial commit

---
+++ src/test/java/com/example/sringboot_mybatis01/SringbootMybatis01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sringboot_mybatis01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringbootMybatis01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
